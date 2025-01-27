be94b538b969d0a122a985b56e1438ef4280bf77
Björn Otto <bjoern.otto@ifak.eu>
2025-01-27 17:29:32 +0100

    Make job fail on error

--- _help_and_materials/check_templates.py
+++ _help_and_materials/check_templates.py
@@ -28,11 +28,7 @@ def find_folders_without_subfolders(base_path) -> Iterator[str]:
             yield root
 
 
-def remove_prefix(s: str, prefix: str) -> str:
-    return s[len(prefix)+1:]
-
-
-def check_template(path: pathlib.Path, root: pathlib.Path):
+def check_template(path: pathlib.Path, root: pathlib.Path) -> bool:
     print(f"Checking {path.relative_to(root)}:")
     try:
         readme = find_one(path, "*.md")
@@ -41,28 +37,34 @@ def check_template(path: pathlib.Path, root: pathlib.Path):
         aasx_files = list(path.glob("*.aasx"))
     except ValueError as e:
         print_red(f"!!! {e}")
-        return
+        return False
+    ok = True
     for i in json_files:
         result = aas_file.check_json_file(open(i))
         if result.ok():
             print_green(f"- {i.relative_to(root)} is ok")
         else:
             print_red(f"- {i.relative_to(root)} is invalid")
+            ok = False
     for i in aasx_files:
         result = aas_file.check_aasx_file(open(i))
         if result.ok():
             print_green(f"- {i.relative_to(root)} is ok")
         else:
             print_red(f"- {i.relative_to(root)} is invalid")
+            ok = False
+    return ok
 
 
 def main() -> int:
     script_dir = pathlib.Path(os.path.dirname(os.path.realpath(__file__)))
     root_dir = script_dir.parent / 'published'
+    ok = True
     for i in find_folders_without_subfolders(root_dir):
         path = pathlib.Path(i)
-        check_template(path, root_dir)
-    return 0
+        if not check_template(path, root_dir):
+            ok = False
+    return 0 if ok else 1
 
 
 if __name__ == "__main__":
